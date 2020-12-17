8e2f157783e3461a445396c2bc2ae76ca7310a21
Sumeet Singh <sumeetsngh0@gmail.com>
2020-12-17 22:37:27 +0530

    Updates

---
+++ precache-manifest.bd80292375a6c1e621c5676371d65d6d.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98393059cd0af5e868f91b4901785b9e",
+    "url": "/portfoliov1/index.html"
+  },
+  {
+    "revision": "384d12a86e236537dc84",
+    "url": "/portfoliov1/static/css/2.70ce2e8c.chunk.css"
+  },
+  {
+    "revision": "7998fdc7f7dcaa44315d",
+    "url": "/portfoliov1/static/css/main.ba2cee92.chunk.css"
+  },
+  {
+    "revision": "384d12a86e236537dc84",
+    "url": "/portfoliov1/static/js/2.a14ef578.chunk.js"
+  },
+  {
+    "revision": "a7ced352984eaea62c27483dbc13303e",
+    "url": "/portfoliov1/static/js/2.a14ef578.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7998fdc7f7dcaa44315d",
+    "url": "/portfoliov1/static/js/main.7c8120dd.chunk.js"
+  },
+  {
+    "revision": "55378f612cb0993a7473",
+    "url": "/portfoliov1/static/js/runtime-main.434968e6.js"
+  },
+  {
+    "revision": "47a9df410a52eb8d51589991548303b3",
+    "url": "/portfoliov1/static/media/Portfolio_full.47a9df41.JPG"
+  },
+  {
+    "revision": "e2f7ae0f8d394dbbd2ed467c53ab202a",
+    "url": "/portfoliov1/static/media/Portfolio_small.e2f7ae0f.JPG"
+  },
+  {
+    "revision": "ea89569ab12c03ccb66a03c6ad92f6e4",
+    "url": "/portfoliov1/static/media/Sumeet's Resume.ea89569a.pdf"
+  },
+  {
+    "revision": "7cfbda7c6c98a9c0d59600c9141aee60",
+    "url": "/portfoliov1/static/media/Yelpcamp_full.7cfbda7c.JPG"
+  },
+  {
+    "revision": "db9a65abef963efabbd99225aae90c20",
+    "url": "/portfoliov1/static/media/Yelpcamp_small.db9a65ab.JPG"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/portfoliov1/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/portfoliov1/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/portfoliov1/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/portfoliov1/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/portfoliov1/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/portfoliov1/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/portfoliov1/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/portfoliov1/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/portfoliov1/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/portfoliov1/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/portfoliov1/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/portfoliov1/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/portfoliov1/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/portfoliov1/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/portfoliov1/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/portfoliov1/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "a6a254aeacf296fc51389ee6f0ca7b7a",
+    "url": "/portfoliov1/static/media/s.a6a254ae.svg"
+  },
+  {
+    "revision": "b4d0daa5ddbb2e9229ab45aa0eb79f13",
+    "url": "/portfoliov1/static/media/undraw_Designer_by46.b4d0daa5.svg"
+  },
+  {
+    "revision": "46d9f22baccdf7718ee608f0b5503c6c",
+    "url": "/portfoliov1/static/media/undraw_coding_6mjf.46d9f22b.svg"
+  },
+  {
+    "revision": "64fb8a6c031e021a72de8ea970588908",
+    "url": "/portfoliov1/static/media/undraw_contact_us_15o2.64fb8a6c.svg"
+  }
+]);
